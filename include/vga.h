660042b54119bfe71caee0e677b221f3a1b9d57f
SolindekDev <solindeklive.biznes@gmail.com>
2022-08-31 19:21:03 +0200

    1.0.1: em lol

--- include/vga.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <multiboot.h>
-
-void vga_init(MULTIBOOT_INFO* mb_info);
-void putchar(char c);
-void printf(const char* str, ...);
-void vga_disable_cursor();
-void vga_debug();
--- m.js
+++
@@ -1,2 +0,0 @@
-for (let i = 0; i < 31; i++)
-    console.log(`idt_set_gate(${i}, (uint32_t)isr${i}, 0x08, 0x8E);`)
